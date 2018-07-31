94a1f7dd5c9c163cf6eb1e3d157b5a6bdf3436dc
Carlos Mota <CarlosMota@protonmail.com>
2018-07-31 11:43:25 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgDragDropModule } from 'ng-drag-drop';
 
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './canvas/canvas.component';
+import { CardViewComponent } from './card-view/card-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatCardModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent,
+    CardViewComponent,
+    MyNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgDragDropModule.forRoot(),
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [CardViewComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/my-nav/my-nav.component.ts
@@ -0,0 +1,29 @@
+import { CardViewComponent } from './../card-view/card-view.component';
+import { Component } from '@angular/core';
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState
+} from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-my-nav',
+  templateUrl: './my-nav.component.html',
+  styleUrls: ['./my-nav.component.css']
+})
+export class MyNavComponent {
+  cardViewComponent = CardViewComponent;
+  component;
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  setCorrectComponent(component) {
+    console.log('component', component.dragData);
+    this.component = component.dragData;
+  }
+}
